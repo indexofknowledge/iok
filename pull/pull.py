985da6a26af3946d6ef103c4175a1c17bacb621b
Rustie (Ting-Chi) Lin <rustielin@berkeley.edu>
2019-10-14 09:03:00 -0700

    init pr helpers

---
+++ pull/pull.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# authentication
+
+# fork the repo (once)
+# curl -u "rustielin" -d "@pull.json" -X POST  https://api.github.com/repos/rustielin/iok/forks
+
+# update personal repo 
+# git branch XYZ
+# git checkout
+# git add 
+# git commit 
+# git push
+
+# get the PR info --> pull.json
+
+# make the PR 
+# curl -u "rustielin" -d "@pull.json" -X POST  https://api.github.com/repos/rustielin/iok/pulls
