5de7192ad8682bbeaa785f9d4aa587c292ce5966
Rustie (Ting-Chi) Lin <rustielin@berkeley.edu>
2020-02-07 17:10:12 -0800

    add new travis stages and deploy targets to firebase

--- deploy_v4.sh
+++ deploy_v4.sh
@@ -3,4 +3,6 @@
 npm install -g firebase-tools
 cd $TRAVIS_BUILD_DIR/v4
 echo "Deploying to only hosting:$DEPLOY_TARGET" 
+npm install
+npm run build
 firebase deploy --project index-of-knowledge --only hosting:$DEPLOY_TARGET --token $FIREBASE_TOKEN
