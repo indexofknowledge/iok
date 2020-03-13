a4420a1c50b7db6accdd3a9d27e12e380276e977
Susan Lin <susanlin@Susans-MacBook-Pro-3.local>
2020-03-12 23:06:29 -0700

    ryan did a lot of work; issue #19

--- v4/src/IokText.js
+++ v4/src/IokText.js
@@ -6,7 +6,7 @@ import { sha256 } from 'js-sha256'
 import './styles/IokText.css'
 
 import { regroupCy, toggleDrawMode, toggleMeta, addNode, getNodesEdgesJson } from './listen'
-import { GRAPH_FILENAME } from './constants'
+import { GRAPH_FILENAME, NTYPE } from './constants'
 
 export default class IokText extends Component {
 
@@ -75,20 +75,20 @@ export default class IokText extends Component {
   addNodeToCy(data) {
     var hash = sha256.create();
     hash.update(JSON.stringify(data))
-    data = {...data, id: hash.hex()}
+    data = { ...data, id: hash.hex() }
     if (!('name' in data)) { // XXX: make a note of this... give it a name...
-      data = {...data, name: 'res-'.concat(data.id.substring(0, 10))}
+      data = { ...data, name: 'res-'.concat(data.id.substring(0, 10)) }
     }
     console.log('DATA', data)
     addNode(this.state.cy, data)
   }
 
-  downloadGraph(){
+  downloadGraph() {
     var exportObj = getNodesEdgesJson(this.state.cy)
     var exportName = GRAPH_FILENAME
     var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
     var downloadAnchorNode = document.createElement('a');
-    downloadAnchorNode.setAttribute("href",     dataStr);
+    downloadAnchorNode.setAttribute("href", dataStr);
     downloadAnchorNode.setAttribute("download", exportName);
     document.body.appendChild(downloadAnchorNode); // required for firefox
     downloadAnchorNode.click();
@@ -96,7 +96,46 @@ export default class IokText extends Component {
   }
 
   render() {
+    const node = this.props.currNode;
+    const data = node ? node.data() : {
+      name: "Overview",
+      data: "Index of Knowledge (IoK) is a curated collection of resources for blockchain, grouped by topic and topologically ordered by pedagogical dependency.", resource_type: 1, node_type: 2,
+    };
+
+    let subtitle = '';
+    if (data.node_type == NTYPE.RESO) {
+      subtitle = 'NOTE: Resource node. Displaying own contents';
+    }
+
+    let neighbors = [data];
+
+    if (data.node_type == NTYPE.TOPIC) {
+      neighbors = node.incomers((el) => el.isNode())
+        .map(neighbor => neighbor.data());
+    }
+
+    const depList = [];
+    const descList = [];
+    const linkList = [];
+
+    for (const neighbor of neighbors) {
+      if (neighbor.node_type === NTYPE.TOPIC) { // topic is dep
+        depList.push(<li>{neighbor.name}</li>);
+      } else if (neighbor.node_type === NTYPE.RESO) { // resource
+        if (neighbor.resource_type === 1) { // desc
+          descList.push(<li>{neighbor.data}</li>);
+        } else { // link type
+          linkList.push(<li><a href={neighbor.data.link}>{neighbor.data.text}</a></li>);
+        }
+      }
+    }
+    //XXX: HACK!! if no node is selected, we use a linkList alongside a descList
+    if (!node) {
+      linkList.push(<li><a href=".">Resource links appear here!</a></li>);
+    }
+
     return (
+
       <div>
 
         <Modal className="Modal" show={this.state.showDeleteModal} onHide={this.toggleDeleteModal}>
@@ -107,7 +146,7 @@ export default class IokText extends Component {
             This will delete all your IoK data from your Blockstack data locker.
           </Modal.Body>
           <Modal.Footer>
-            <Button className={"btn btn-info btn-lg btn-delete"} onClick={() => {this.onDeleteClick(); this.toggleDeleteModal()}}>
+            <Button className={"btn btn-info btn-lg btn-delete"} onClick={() => { this.onDeleteClick(); this.toggleDeleteModal() }}>
               Delete
             </Button>
           </Modal.Footer>
@@ -118,82 +157,82 @@ export default class IokText extends Component {
             <Modal.Title>Save your IoK?</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            This will save your IoK data to your Blockstack data locker, potentially overwriting previous versions. 
+            This will save your IoK data to your Blockstack data locker, potentially overwriting previous versions.
           </Modal.Body>
           <Modal.Footer>
-            <Button className={"btn btn-info btn-lg btn-save"} onClick={() => {this.onSaveClick(); this.toggleSaveModal()}}>
+            <Button className={"btn btn-info btn-lg btn-save"} onClick={() => { this.onSaveClick(); this.toggleSaveModal() }}>
               Save
             </Button>
           </Modal.Footer>
         </Modal>
 
         <div id="cytext" className="iok-text">
+          <div>
+            <h2 className="breaking" id="nodetitle">{data.name}</h2>
+
+            {/* used mainly to display info text to user */}
+            <p className="breaking" id="nodeid">{data.id}</p>
+            <p id="nodesubtitle">{subtitle}</p>
+
+            <hr className="hr-sep"></hr>
+
             <div>
-                <h2 className="breaking" id="nodetitle">Overview</h2>
+              <h5>Description(s)</h5>
+              <ul id="nodedescs">
+                {descList}
+              </ul>
+            </div>
 
-                {/* used mainly to display info text to user */}
-                <p className="breaking" id="nodeid"></p>
-                <p id="nodesubtitle"></p>
+            <div>
+              <h5>Links</h5>
+              <ul id="nodelinks">
+                {linkList}
+              </ul>
+            </div>
 
-                <hr className="hr-sep"></hr>
-        
-                <div>
-                  <h5>Description(s)</h5>
-                  <ul id="nodedescs">
-                    <li>Index of Knowledge (IoK) is a curated collection of resources for blockchain, grouped by topic and topologically ordered by pedagogical dependency.</li>
-                  </ul>
-                </div>
-        
-                <div>
-                  <h5>Links</h5>
-                  <ul id="nodelinks">
-                      <li><a href=".">Resource links appear here!</a></li>
-                  </ul>
-                </div>
+            <div>
+              <h5>Dependencies</h5>
+              <ul id="nodedeps">
+                {depList}
+              </ul>
+            </div>
 
-                <div>
-                  <h5>Dependencies</h5>
-                  <ul id="nodedeps">
-                      <li>placeholder dep</li>
-                  </ul>
-                </div>
+            <hr className="hr-sep"></hr>
 
-                <hr className="hr-sep"></hr>
-
-                <div>
-                  {/* <div>
+            <div>
+              {/* <div>
                     <h5>Misc IoK</h5>
                     <button className="btn btn-info btn-lg btn-util" onClick={this.onMetaClick}>Toggle meta-graph</button>
                     <button className="btn btn-info btn-lg btn-util" onClick={this.onRegroupClick}>Regroup</button>
                   </div> */}
 
-                  <div className="edit-div">
-                    <h5>Edit</h5>
-                    <AddNodeModal addNode={this.addNodeToCy}/>
+              <div className="edit-div">
+                <h5>Edit</h5>
+                <AddNodeModal addNode={this.addNodeToCy} />
 
-                    {this.props.guestMode ? <div></div> : 
-                    <div><button className="btn btn-info btn-lg btn-save" onClick={this.toggleSaveModal}>Save</button>
+                {this.props.guestMode ? <div></div> :
+                  <div><button className="btn btn-info btn-lg btn-save" onClick={this.toggleSaveModal}>Save</button>
                     <button className="btn btn-info btn-lg btn-delete" onClick={this.toggleDeleteModal}>Delete</button></div>
-                    }
-                    
-                    <button 
-                      className="btn btn-info btn-lg btn-util" 
-                      onClick={
-                        () => {
-                          this.onDrawClick(); 
-                          this.setState({drawEnabled: !this.state.drawEnabled});
-                          return
-                        }
-                      }
-                    >
-                      Turn {this.state.drawEnabled ? 'OFF' : 'ON'} edge drawing
-                    </button>
-                    {this.props.graphLoaded ? <button id="downloadButton" className="btn btn-info btn-lg btn-util" onClick={this.downloadGraph}>Download</button> : <div></div>}
-                    
-                  </div>
+                }
 
-                </div>
+                <button
+                  className="btn btn-info btn-lg btn-util"
+                  onClick={
+                    () => {
+                      this.onDrawClick();
+                      this.setState({ drawEnabled: !this.state.drawEnabled });
+                      return
+                    }
+                  }
+                >
+                  Turn {this.state.drawEnabled ? 'OFF' : 'ON'} edge drawing
+                    </button>
+                {this.props.graphLoaded ? <button id="downloadButton" className="btn btn-info btn-lg btn-util" onClick={this.downloadGraph}>Download</button> : <div></div>}
+
+              </div>
+
             </div>
+          </div>
         </div>
       </div>
     );
