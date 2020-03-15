ce7c603930a7438727cca2fb7888c5afffc075d2
Rustie (Ting-Chi) Lin <rustielin@berkeley.edu>
2020-03-14 20:11:29 -0700

    lint index

---
+++ v4/src/index.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+// Require Sass file so webpack can build it
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles/style.css';
+
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
