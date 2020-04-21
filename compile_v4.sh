6cd18762ccbe0e0cb21d4d7de9eceaf455711e05
Rustie (Ting-Chi) Lin <rustielin@berkeley.edu>
2020-04-20 21:17:00 -0700

    [travis] skip compile and deploy steps if no changes, also bump node to v13

--- compile_v4.sh
+++ compile_v4.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-cd $TRAVIS_BUILD_DIR/v4
-npm install
-npm run lint && npm run build
+git diff-tree --name-only -r HEAD~1 | grep v4 && do_compile=1
+
+if [ -n "$do_compile" ]; then
+    cd $TRAVIS_BUILD_DIR/v4
+    npm install
+    npm run lint && npm run build
+else
+    echo "No changes, skipping build!"
+fi
--- deploy_v4.sh
+++ deploy_v4.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-npm install -g firebase-tools
-cd $TRAVIS_BUILD_DIR/v4
-echo "Deploying to only hosting:$DEPLOY_TARGET" 
-npm install
-npm run build
-firebase deploy --project index-of-knowledge --only hosting:$DEPLOY_TARGET --token $FIREBASE_TOKEN
+git diff-tree --name-only -r HEAD~1 | grep v4 && do_deploy=1
+
+if [ -n "$do_deploy" ]; then
+    npm install -g firebase-tools
+    cd $TRAVIS_BUILD_DIR/v4
+    echo "Deploying to only hosting:$DEPLOY_TARGET" 
+    npm install
+    npm run build
+    firebase deploy --project index-of-knowledge --only hosting:$DEPLOY_TARGET --token $FIREBASE_TOKEN
+else
+    echo "No changes, skipping deploy!"
+fi
