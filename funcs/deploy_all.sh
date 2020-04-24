791d124f1723069c6ba4a47936881a2eba24e1e6
Rustie (Ting-Chi) Lin <rustielin@berkeley.edu>
2020-04-23 22:36:24 -0700

    refactor payload and allow cors

--- funcs/deploy_all.sh
+++ funcs/deploy_all.sh
@@ -3,18 +3,8 @@
 pwd=$PWD
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-###########
-# Just deploy everything
-###########
-
-cd $DIR/retrieve_graph
-gcloud functions deploy retrieveGraph --runtime nodejs8 --trigger-http --allow-unauthenticated --env-vars-file ../.env.yaml
-
 cd $DIR/store_graph
 gcloud functions deploy storeGraph --runtime nodejs8 --trigger-http --allow-unauthenticated --env-vars-file ../.env.yaml
-
-cd $DIR/test_auth
-gcloud functions deploy helloHttp --runtime nodejs8 --trigger-http --allow-unauthenticated --env-vars-file ../.env.yaml
 
 
 cd $pwd
--- funcs/store_graph/index.js
+++ funcs/store_graph/index.js
@@ -1,6 +1,8 @@
 const pinataSDK = require('@pinata/sdk');
 const pinata = pinataSDK(process.env.PINATA_API_KEY, process.env.PINATA_SECRET_KEY);
 
+const cors = require('cors')
+
 /**
  * HTTP Cloud Function.
  *
@@ -9,14 +11,23 @@ const pinata = pinataSDK(process.env.PINATA_API_KEY, process.env.PINATA_SECRET_K
  * @param {Object} res Cloud Function response context.
  *                     More info: https://expressjs.com/en/api.html#res
  */
+
 exports.storeGraph = (req, res) => {
-    if (!('elements' in req.body)) {
-        res.send("ERROR: Invalid graph");
-    }
-    body = {"elements": req.body.elements};
-    pinata.pinJSONToIPFS(body).then((result) => {
-        res.send(result);
-    }).catch((err) => {
-        res.send("ERROR: Could not pin");
+    cors_inst = cors();
+    cors_inst(req, res, () => {
+        storeGraph(req, res)
     })
+}
+
+const storeGraph = (req, res) => {
+      console.log("PAYLOAD BODY", req.body);
+      if (!req.body.hasOwnProperty('elements')) {
+          res.send("ERROR: Invalid graph");
+      }
+      body = {"elements": req.body.elements};
+      pinata.pinJSONToIPFS(body).then((result) => {
+          res.send(result);
+      }).catch((err) => {
+          res.send("ERROR: Could not pin");
+      })
 };
