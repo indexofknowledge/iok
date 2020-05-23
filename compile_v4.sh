f0d11a149b0a68ec20e551cd93d259a52edcb3c1
Rustie (Ting-Chi) Lin <rustielin@berkeley.edu>
2020-05-22 17:35:28 -0700

    [travis] remove git diff checks on subdirectories

--- compile_v4.sh
+++ compile_v4.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-git diff-tree --name-only -r HEAD~1 | grep v4 && do_compile=1
-
-if [ -n "$do_compile" ]; then
-    cd $TRAVIS_BUILD_DIR/v4
-    npm install
-    npm run lint && npm run build
-else
-    echo "No changes, skipping build!"
-fi
+cd $TRAVIS_BUILD_DIR/v4
+npm install
+npm run lint && npm run build
