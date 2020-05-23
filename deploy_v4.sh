dd3ec4c4d9f87b86d85bdb6d8fbf21c4a9d420a6
Rustie (Ting-Chi) Lin <rustielin@berkeley.edu>
2020-05-22 17:35:28 -0700

    [travis] remove git diff checks on subdirectories

--- deploy_v4.sh
+++ deploy_v4.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-git diff-tree --name-only -r HEAD~1 | grep v4 && do_deploy=1
-
-if [ -n "$do_deploy" ]; then
-    npm install -g firebase-tools
-    cd $TRAVIS_BUILD_DIR/v4
-    echo "Deploying to only hosting:$DEPLOY_TARGET" 
-    npm install
-    npm run build
-    firebase deploy --project index-of-knowledge --only hosting:$DEPLOY_TARGET --token $FIREBASE_TOKEN
-else
-    echo "No changes, skipping deploy!"
-fi
+npm install -g firebase-tools
+cd $TRAVIS_BUILD_DIR/v4
+echo "Deploying to only hosting:$DEPLOY_TARGET" 
+npm install
+npm run build
+firebase deploy --project index-of-knowledge --only hosting:$DEPLOY_TARGET --token $FIREBASE_TOKEN
