cc1270dfe0ae929d8b44b134cf2efed9d06a0ecb
Rustie (Ting-Chi) Lin <rustielin@berkeley.edu>
2020-05-26 18:33:36 -0700

    [scraper] deprecate puppeteer for blockstack and instead go to ipfs gateway

--- deploy_scraper.sh
+++ deploy_scraper.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export SCRAPE_LINK="https://ipfs.io/ipfs/QmfA4hp3ue2JS2BR2hG18rWgVCWxDFaRYaGV4GQm4WsrdT"
+
 # log the python version
 python --version
 
@@ -9,15 +11,8 @@ sudo apt-get update
 sudo apt-get install python3.7
 sudo apt-get install python3-pip
 
-# implicit puppeteer dependencies
-sudo apt install -y gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
-
 cd $TRAVIS_BUILD_DIR/scraper 
 
 python3.7 -m pip install -r requirements.txt 
 
-npm install puppeteer 
-npm install
-
-# run the scraper
-./scrape-awesome.sh -v
+python3 scrape.py --link $SCRAPE_LINK --out ../README.md
---
+++ scraper/scrape.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import requests
+import argparse
+import sys
+from iok import KnowledgeGraph, AwesomeClient
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--link", type=str, required=True, help="Link to IoK JSON graph file")
+parser.add_argument("--out", type=str, required=True, help="Path of the output file")
+parser.add_argument('--debug', action='store_true')
+
+args = parser.parse_args()
+
+r = requests.get(args.link)
+graph = r.json()
+
+g = KnowledgeGraph(obj=graph, debug=args.debug)
+
+a = AwesomeClient(g)
+a.build_map()
+a.write_to_file(args.out)
