cf49157de09b7eb4f8a0c305d666cd8f7c987f76
Rustie (Ting-Chi) Lin <rustielin@berkeley.edu>
2020-05-26 20:57:15 -0700

    [refactor] add scraper test

--- src/scraper/scrape.py
+++ src/scraper/scrape.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python
 
 import requests
-import argparse
 import sys
 from iok.meta import KnowledgeGraph, AwesomeClient
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--link", type=str, required=True, help="Link to IoK JSON graph file"
-)
-parser.add_argument("--out", type=str, required=True, help="Path of the output file")
-parser.add_argument("--debug", action="store_true")
+def run_scrape(link: str, out_file: str, debug: bool = False) -> None:
+    r = requests.get(link)
+    graph = r.json()
 
-args = parser.parse_args()
+    g = KnowledgeGraph(obj=graph, debug=debug)
 
-r = requests.get(args.link)
-graph = r.json()
+    a = AwesomeClient(g)
+    a.build_map()
 
-g = KnowledgeGraph(obj=graph, debug=args.debug)
-
-a = AwesomeClient(g)
-a.build_map()
-a.write_to_file(args.out)
+    if out_file:
+        a.write_to_file(out_file)
+    else:
+        print(a.build_str())
---
+++ src/scraper/tests/test_data.py
@@ -0,0 +1,248 @@
+TEST_DATA = {
+    "elements": {
+        "nodes": [
+            {
+                "data": {
+                    "resource_type": 2,
+                    "data": {
+                        "link": "https://www.wikihow.com/Grow-a-Pineapple",
+                        "text": "Grow Pineapple [WikiHow]",
+                    },
+                    "node_type": 2,
+                    "id": "edb4bb55eb38b17bd0591e3f17ff88c4e213088da2019f3770cb9257bec0ed6b",
+                    "name": "res-edb4bb55eb",
+                }
+            },
+            {
+                "data": {
+                    "resource_type": 3,
+                    "data": {
+                        "link": "https://www.youtube.com/watch?v=OpvPGQI1xj8",
+                        "text": "Grow a Pineapple",
+                    },
+                    "node_type": 2,
+                    "id": "f40c045d05ff64a4739a9897bea0491a9c8f6907257da0f3a7c6e926d1df056f",
+                    "name": "res-f40c045d05",
+                }
+            },
+            {
+                "data": {
+                    "resource_type": 2,
+                    "data": {
+                        "link": "https://www.foodnetwork.com/how-to/articles/how-to-make-a-basic-tomato-sauce-a-step-by-step-guide",
+                        "text": "Tomato Sauce [food network]",
+                    },
+                    "node_type": 2,
+                    "id": "87f54181376bbd4f8c6f288161d0458fb31d47234ed4380b90606fb0261954f5",
+                    "name": "res-87f5418137",
+                }
+            },
+            {
+                "data": {
+                    "resource_type": 2,
+                    "data": {
+                        "link": "https://www.bonappetit.com/recipe/classic-pesto-sauce",
+                        "text": "Pesto Sauce [Bon Appetit]",
+                    },
+                    "node_type": 2,
+                    "id": "d311defaa900fcf915ed1bdb13fdf3144a9e460294d2f4db8d5dad7daf75e96c",
+                    "name": "res-d311defaa9",
+                }
+            },
+            {
+                "data": {
+                    "resource_type": 2,
+                    "data": {
+                        "link": "https://www.allrecipes.com/recipe/20171/quick-and-easy-pizza-crust/",
+                        "text": "Pizza Crust [All Recipes]",
+                    },
+                    "node_type": 2,
+                    "id": "2021eb9866f9185f074757f1671552235de58fb4774f5cf71d1237a81bf7fb92",
+                    "name": "res-2021eb9866",
+                }
+            },
+            {
+                "data": {
+                    "resource_type": 2,
+                    "data": {
+                        "link": "https://damndelicious.net/2014/07/05/easy-homemade-pesto/",
+                        "text": "Pesto Sauce [Damn Delicious]",
+                    },
+                    "node_type": 2,
+                    "id": "09e510c8b3be2319b2d6d773eaafd3e3678300ae091ba9f11d79f99df7143bb6",
+                    "name": "res-09e510c8b3",
+                }
+            },
+            {
+                "data": {
+                    "name": "Pineapple",
+                    "node_type": 1,
+                    "id": "589a6687be411882a7e7fd16183ce304097be17f4d9ec2024c99d10e2e832908",
+                }
+            },
+            {
+                "data": {
+                    "name": "Pizza",
+                    "node_type": 1,
+                    "id": "766dd26814a75aed6dfbae1697a1e6d75e2070cf937ed466bbbba66ae73bbeba",
+                }
+            },
+            {
+                "data": {
+                    "name": "Crust",
+                    "node_type": 1,
+                    "id": "ab4554f154dcfbcf9c912948edd466e201f151f6ac03c7c0151685b4354ba271",
+                }
+            },
+            {
+                "data": {
+                    "name": "Sauce",
+                    "node_type": 1,
+                    "id": "a84b4438be46222f040ec0632fcc0578fa2f0506d717523f7f840431518f78c9",
+                }
+            },
+            {
+                "data": {
+                    "name": "Toppings",
+                    "node_type": 1,
+                    "id": "734d68327849dedd564b13465d327fbbd82fd8c0d55aaaaf49fd9f2132dcf4d4",
+                }
+            },
+            {
+                "data": {
+                    "name": "Tomato Sauce",
+                    "node_type": 1,
+                    "id": "f82e9c968c1364fad9c576ce23bef18310bfe5492480ba903d39e8cc6180a6fd",
+                }
+            },
+            {
+                "data": {
+                    "name": "Pesto Sauce",
+                    "node_type": 1,
+                    "id": "b4155a2c837b92532652fd70ff114af353e78fd0d9b07b90e88cdd6c6a578869",
+                }
+            },
+            {
+                "data": {
+                    "resource_type": 2,
+                    "data": {
+                        "link": "https://damndelicious.net/2020/03/14/quick-tomato-sauce/",
+                        "text": "Tomato Sauce [Damn Delicious]",
+                    },
+                    "node_type": 2,
+                    "id": "7bfbfcfc0eca99bcd7aa626283289e8d594b9ca9174f2443afcc0c57e8e5fee1",
+                    "name": "res-7bfbfcfc0e",
+                }
+            },
+            {
+                "data": {
+                    "resource_type": 1,
+                    "data": "a dish of Italian origin consisting of a flat, round base of dough baked with a topping of tomato sauce and cheese, typically with added meat or vegetables.",
+                    "node_type": 2,
+                    "id": "512b6a3b60506b05da1d61426bd3214b58c2765d29c1db8d76460d23c3a40373",
+                    "name": "res-512b6a3b60",
+                }
+            },
+        ],
+        "edges": [
+            {
+                "data": {
+                    "source": "87f54181376bbd4f8c6f288161d0458fb31d47234ed4380b90606fb0261954f5",
+                    "target": "f82e9c968c1364fad9c576ce23bef18310bfe5492480ba903d39e8cc6180a6fd",
+                    "id": "657dedba-a9e3-4a15-95b7-cad762bb1e79",
+                }
+            },
+            {
+                "data": {
+                    "source": "7bfbfcfc0eca99bcd7aa626283289e8d594b9ca9174f2443afcc0c57e8e5fee1",
+                    "target": "f82e9c968c1364fad9c576ce23bef18310bfe5492480ba903d39e8cc6180a6fd",
+                    "id": "07b3a342-5c43-4242-8c10-243fd4204b9b",
+                }
+            },
+            {
+                "data": {
+                    "source": "f82e9c968c1364fad9c576ce23bef18310bfe5492480ba903d39e8cc6180a6fd",
+                    "target": "a84b4438be46222f040ec0632fcc0578fa2f0506d717523f7f840431518f78c9",
+                    "id": "65bee2fd-a9b3-4620-9c79-87bad8eb31cc",
+                }
+            },
+            {
+                "data": {
+                    "source": "a84b4438be46222f040ec0632fcc0578fa2f0506d717523f7f840431518f78c9",
+                    "target": "766dd26814a75aed6dfbae1697a1e6d75e2070cf937ed466bbbba66ae73bbeba",
+                    "id": "caa6de4c-e228-4bc8-b5c0-56738c68587b",
+                }
+            },
+            {
+                "data": {
+                    "source": "ab4554f154dcfbcf9c912948edd466e201f151f6ac03c7c0151685b4354ba271",
+                    "target": "766dd26814a75aed6dfbae1697a1e6d75e2070cf937ed466bbbba66ae73bbeba",
+                    "id": "5e102298-b21b-41b0-b23a-3fe89aa205d7",
+                }
+            },
+            {
+                "data": {
+                    "source": "589a6687be411882a7e7fd16183ce304097be17f4d9ec2024c99d10e2e832908",
+                    "target": "734d68327849dedd564b13465d327fbbd82fd8c0d55aaaaf49fd9f2132dcf4d4",
+                    "id": "3bf0b6d4-7069-4178-9ae6-44d7cd621086",
+                }
+            },
+            {
+                "data": {
+                    "source": "edb4bb55eb38b17bd0591e3f17ff88c4e213088da2019f3770cb9257bec0ed6b",
+                    "target": "589a6687be411882a7e7fd16183ce304097be17f4d9ec2024c99d10e2e832908",
+                    "id": "924b555b-4a60-4bb0-998b-6dbd90803a2b",
+                }
+            },
+            {
+                "data": {
+                    "source": "2021eb9866f9185f074757f1671552235de58fb4774f5cf71d1237a81bf7fb92",
+                    "target": "ab4554f154dcfbcf9c912948edd466e201f151f6ac03c7c0151685b4354ba271",
+                    "id": "ba830e29-8f29-4c6e-9fb0-a7898c74cb38",
+                }
+            },
+            {
+                "data": {
+                    "source": "d311defaa900fcf915ed1bdb13fdf3144a9e460294d2f4db8d5dad7daf75e96c",
+                    "target": "b4155a2c837b92532652fd70ff114af353e78fd0d9b07b90e88cdd6c6a578869",
+                    "id": "6dfad5a1-58ca-4d0f-a81b-ee8a495ba31c",
+                }
+            },
+            {
+                "data": {
+                    "source": "09e510c8b3be2319b2d6d773eaafd3e3678300ae091ba9f11d79f99df7143bb6",
+                    "target": "b4155a2c837b92532652fd70ff114af353e78fd0d9b07b90e88cdd6c6a578869",
+                    "id": "5a10846b-0f90-4052-a067-1e95ae8d5995",
+                }
+            },
+            {
+                "data": {
+                    "source": "f40c045d05ff64a4739a9897bea0491a9c8f6907257da0f3a7c6e926d1df056f",
+                    "target": "589a6687be411882a7e7fd16183ce304097be17f4d9ec2024c99d10e2e832908",
+                    "id": "c6214abb-c06d-429d-a1d9-01d6a42e203c",
+                }
+            },
+            {
+                "data": {
+                    "source": "734d68327849dedd564b13465d327fbbd82fd8c0d55aaaaf49fd9f2132dcf4d4",
+                    "target": "766dd26814a75aed6dfbae1697a1e6d75e2070cf937ed466bbbba66ae73bbeba",
+                    "id": "eacbbcdb-0261-439f-af13-b4c2d6cae728",
+                }
+            },
+            {
+                "data": {
+                    "source": "b4155a2c837b92532652fd70ff114af353e78fd0d9b07b90e88cdd6c6a578869",
+                    "target": "a84b4438be46222f040ec0632fcc0578fa2f0506d717523f7f840431518f78c9",
+                    "id": "1e6efe1a-f7bb-423e-90ed-299ca78b0eb3",
+                }
+            },
+            {
+                "data": {
+                    "source": "512b6a3b60506b05da1d61426bd3214b58c2765d29c1db8d76460d23c3a40373",
+                    "target": "766dd26814a75aed6dfbae1697a1e6d75e2070cf937ed466bbbba66ae73bbeba",
+                    "id": "5f58366b-ceed-46e1-9ba6-a1204932f949",
+                }
+            },
+        ],
+    }
+}
