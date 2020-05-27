2d89ca60f0d647864a0442dd06b46ff60e26ec3c
Rustie (Ting-Chi) Lin <rustielin@berkeley.edu>
2020-05-26 20:57:15 -0700

    [refactor] add scraper test

---
+++ src/scraper/tests/test_scrape.py
@@ -0,0 +1,18 @@
+import requests
+import json
+import os, sys
+
+from _pytest.monkeypatch import MonkeyPatch
+from ..scrape import run_scrape
+from .test_data import TEST_DATA
+
+
+class MockRequestGet:
+    def json(self):
+        return TEST_DATA
+
+
+def test_scape_populates_meta(monkeypatch: MonkeyPatch) -> None:
+
+    monkeypatch.setattr(requests, "get", lambda x: MockRequestGet())
+    run_scrape("fakelink", None)
