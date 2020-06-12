06d6765bda4279a1fc4a23e3d0729e119d41b891
Susan Lin <susan.lin@berkeley.edu>
2020-06-12 10:16:36 -0700

    shortened md_scraper and fixed lint issues

--- v4/src/md_scraper.js
+++ v4/src/md_scraper.js
@@ -1,7 +1,6 @@
-// import { sha256 } from 'js-sha256';
-const sha256 = require('js-sha256');
+const sha256 = require('js-sha256'); // eslint-disable-line
 
-// regex for ordering lines based on hierarchy 
+// regex for ordering lines based on hierarchy
 const TOPIC = /^(#+)\s/;
 const TEXT = /^[A-Za-z]/;
 const RESOURCES = [TEXT, /^[*-]/];
@@ -15,7 +14,7 @@ class Scope {
     this.line = line.trim();
 
     // create node
-    if (match != TEXT) {
+    if (match !== TEXT) {
       this.line = line.replace(match, '').trim();
     }
     const isTopic = (match === TOPIC);
@@ -30,7 +29,7 @@ class Scope {
     // create id
     const hash = sha256.create();
     hash.update(JSON.stringify(this.node));
-    this.node.id = this.id = hash.hex();
+    this.node.id = this.id = hash.hex(); // eslint-disable-line
 
     // set level for comparing hierarchy
     this.level = (match === TOPIC) ? line.match(TOPIC)[1].length : Infinity;
@@ -40,100 +39,103 @@ class Scope {
     if (!this.data) return this.line;
     return this.data.text.slice(0, 10);
   }
-
+  /* eslint-disable */
   nodeData() {
     if (this.match === TOPIC) return;
-    let link = this.line.match(/(?:__|[*#])|\[(.*?)\]\((.*?)\)/);
+    const link = this.line.match(/(?:__|[*#])|\[(.*?)\]\((.*?)\)/);
     if (link && link[0] && link[1]) {
-      return { text: link[1], link: link[2] }
-    } else {
-      return { text: this.line, link: '' };
+      return { text: link[1], link: link[2] };
     }
+    return { text: this.line, link: '' };
   }
+  /* eslint-enable */
 }
 
-// Match the given line with the regex hierarchy, returning the most specific match    
+// Match the given line with the regex hierarchy, returning the most specific match
 function matchHierarchy(line) {
-  return HIERARCHY.find(h => h.test(line));
+  return HIERARCHY.find((h) => h.test(line));
 }
 
 // Update the scopes and add a new node to graph
-function updateScopes(new_scope, scopes, graph) {
-  let scopes_cpy = [...scopes];
+function updateScopes(newScope, scopes, graph) {
+  // eslint-disable-next-line
+  let scopesCpy = [...scopes];
 
-  graph.nodes.push(new_scope.node);
+  graph.nodes.push(newScope.node);
   // console.log("Adding ", new_scope.node, "\n")
 
-  if (scopes_cpy.length) {
-    let target_id = scopes_cpy[scopes_cpy.length - 1].id;
+  if (scopesCpy.length) {
+    const targetId = scopesCpy[scopesCpy.length - 1].id;
     // console.log("Adding edge ", new_scope.id, " AND ", target_id, "\n")
 
     const hash = sha256.create();
-    hash.update(new_scope.id + target_id);
+    hash.update(newScope.id + targetId);
 
     graph.edges.push(
       {
-        source: new_scope.id,
-        target: target_id,
+        source: newScope.id,
+        target: targetId,
         id: hash.hex(),
       },
     );
   }
-  scopes_cpy.push(new_scope);
-  return scopes_cpy;
+  scopesCpy.push(newScope);
+  return scopesCpy;
 }
 
 /**
- * Based on the current scope (list of regex by increasing specificity), 
- * add the current matched line to the graph by either branching or continuing the current branch 
+ * Based on the current scope (list of regex by increasing specificity),
+ * add the current matched line to the graph by either branching or continuing the current branch
  */
-function matchLine(new_scope, scopes, graph) {
-  let scopes_cpy = [...scopes];
+function matchLine(newScope, scopes, graph) {
+  // eslint-disable-next-line
+  let scopesCpy = [...scopes];
 
-  if (!new_scope.match) {
-    return scopes_cpy;
+  if (!newScope.match) {
+    return scopesCpy;
   }
 
-  if (!scopes_cpy.length) {
-    return updateScopes(new_scope, scopes, graph);
+  if (!scopesCpy.length) {
+    return updateScopes(newScope, scopes, graph);
   }
 
-  const end_scope = scopes_cpy[scopes_cpy.length - 1];
+  const endScope = scopesCpy[scopesCpy.length - 1];
 
-  if (new_scope.level > end_scope.level) {
-    return updateScopes(new_scope, scopes, graph);
-  } else {
-    scopes_cpy.pop()
-    return matchLine(new_scope, scopes_cpy, graph);
+  if (newScope.level > endScope.level) {
+    return updateScopes(newScope, scopes, graph);
   }
+  scopesCpy.pop();
+  return matchLine(newScope, scopesCpy, graph);
 }
 
-//given a string, returns the graph in IOK format
+// given a string, returns the graph in IOK format
 function createGraph(text) {
-  let textlines = text.split('\n');
+  const textlines = text.split('\n');
   let scopes = [];
 
+  // eslint-disable-next-line
   let graph = {
     nodes: [],
     edges: [],
   };
 
+  // eslint-disable-next-line
   for (const line of textlines) {
     const match = matchHierarchy(line);
-    const new_scope = new Scope(match, line);
-    scopes = matchLine(new_scope, scopes, graph);
+    const newScope = new Scope(match, line);
+    scopes = matchLine(newScope, scopes, graph);
   }
   return graph;
 }
 
-export function graph_from_url(link) {
-  const text = fetch(link).then(response => response.text());
+export function graphFromUrl(link) {
+  const text = fetch(link).then((response) => response.text());
   const g = createGraph(text);
   // console.log(JSON.stringify(g, null, 2));
   return g;
 }
 
-export function graph_from_text(text) {
+export function graphFromText(text) {
   const g = createGraph(text);
   // console.log(JSON.stringify(g, null, 2));
   return g;
--- v4/src/md_scraper.test.js
+++ v4/src/md_scraper.test.js
@@ -1,256 +1,148 @@
 /* eslint-disable */
-import { graph_from_text } from './md_scraper'
+import { graphFromText } from './md_scraper'
 
+function createTestNode(nodeType, name, id, resType, text, link) {
+  if (nodeType === 1) {
+    return {
+      "id": expect.toStartWith(id),
+      "name": name,
+      "node_type": nodeType,
+    }
+  } else if (nodeType === 2) {
+    return {
+      "data": {
+        "text": text,
+        "link": link,
+      },
+      "id": expect.toStartWith(id),
+      "name": name,
+      "node_type": nodeType,
+      "resource_type": resType,
+    }
+  }
+  return null
+}
+
+function createTestEdge(id, source, target) {
+  return {
+    id: expect.toStartWith(id),
+    source: expect.toStartWith(source),
+    target: expect.toStartWith(target),
+  }
+}
+
+//compare just the beginning for id hashes
+expect.extend({
+  toStartWith(received, beginning) {
+    return {
+      message: () =>
+        `${received} should equal ${beginning}`,
+      pass: beginning.slice(0, 10) === (received.slice(0, 10)),
+
+    }
+  }
+});
+
+//Tests
 describe('markdown scraper', () => {
 
   it('should parse an empty markdown', () => {
-    expect(graph_from_text('')).toEqual({
+    expect(graphFromText('')).toEqual({
       nodes: [],
       edges: [],
     });
   });
 
   it('only description resource nodes', () => {
-    expect(graph_from_text(
+    expect(graphFromText(
       '- FirstRes \n-SecondRes \nThirdRes'
     )).toEqual({
       nodes: [
-        {
-          "data": {
-            "text": "FirstRes",
-            "link": "",
-          },
-          "id": "1e71a7e10dd511b4937b4de28665cb2cdefcf99526e987931a355ab9ce82809a",
-          "name": "FirstRes",
-          "node_type": 2,
-          "resource_type": 1,
-        },
-        {
-          "data": {
-            "text": "SecondRes",
-            "link": "",
-          },
-          "id": "4faea3d4eb412cea485e9e70c5f8a64b4af3690b5a9c9488bc7a3a0e065459d7",
-          "name": "SecondRes",
-          "node_type": 2,
-          "resource_type": 1,
-        },
-        {
-          "data": {
-            "text": "ThirdRes",
-            "link": "",
-          },
-          "id": "c0dd146d647e4eeded368dcae8030c55bbf1ffa7516399deca7b918c82e2c343",
-          "name": "ThirdRes",
-          "node_type": 2,
-          "resource_type": 1,
-        },
+        createTestNode(2, "FirstRes", "1e71a7e10dd511b4937b4de28", 1, "FirstRes", ""),
+        createTestNode(2, "SecondRes", "4faea3d4eb412cea485e9e70c5f8a6", 1, "SecondRes", ""),
+        createTestNode(2, "ThirdRes", "c0dd146d647e4eeded368dcae8030c55bbf", 1, "ThirdRes", ""),
       ],
       edges: [],
     })
   });
 
   it('only link resource nodes', () => {
-    expect(graph_from_text(
+    expect(graphFromText(
       `- [linkTitle1](insertLink1) extra
       \n- [linkTitle2](insertLink2) extra text
       \n- [linkTitle3](insertLink3) extra text idk`
     )).toEqual({
       nodes: [
-        {
-          "data": {
-            "link": "insertLink1",
-            "text": "linkTitle1",
-          },
-          "id": "40f38118359d044116aa7954b8c223b50081b62f26384b0f082223739e216032",
-          "name": "linkTitle1",
-          "node_type": 2,
-          "resource_type": 2,
-        },
-        {
-          "data": {
-            "link": "insertLink2",
-            "text": "linkTitle2",
-          },
-          "id": "4b9abb860dda2bc8fa41e2ed036c7fa6c19038ef694917424343d328efa6c6b7",
-          "name": "linkTitle2",
-          "node_type": 2,
-          "resource_type": 2,
-        },
-        {
-          "data": {
-            "link": "insertLink3",
-            "text": "linkTitle3",
-          },
-          "id": "8dfd191644db73fe0aebf493432ec81b9fe9005124633e51de49889420cefb18",
-          "name": "linkTitle3",
-          "node_type": 2,
-          "resource_type": 2,
-        },
+        createTestNode(2, "linkTitle1", "40f38118359d044116aa7954b8c223b5", 2, "linkTitle1", "insertLink1"),
+        createTestNode(2, "linkTitle2", "4b9abb860dda2bc8fa41e2ed036c7fa", 2, "linkTitle2", "insertLink2"),
+        createTestNode(2, "linkTitle3", "8dfd191644db73fe0aebf493432ec81", 2, "linkTitle3", "insertLink3"),
       ],
       edges: [],
     });
   });
 
   it('only same level topic nodes', () => {
-    expect(graph_from_text(
+    expect(graphFromText(
       '# First Topic \n# Second Topic \n# Third Topic'
     )).toEqual({
       "nodes": [
-        {
-          "id": "e00194285639baedc6dc178503819739df8fc8bd504bd83d115bb3f64c530d3a",
-          "name": "First Topic",
-          "node_type": 1,
-        },
-        {
-          "id": "6890869a8baf91bbf01ef426bcf38b638e13409df7ff490ddd05c68d6d09d2c8",
-          "name": "Second Topic",
-          "node_type": 1,
-        },
-        {
-          "id": "dcd9cbfb54e9dbcff23b828abf4d538224b0005d892beae877872c007eea3f14",
-          "name": "Third Topic",
-          "node_type": 1,
-        },
+        createTestNode(1, "First Topic", "e00194285639baedc6dc1785038", null, null, null),
+        createTestNode(1, "Second Topic", "6890869a8baf91bbf01ef4", null, null, null),
+        createTestNode(1, "Third Topic", "dcd9cbfb54e9dbcff2", null, null, null)
       ],
       edges: []
     })
   });
 
   it('same (higher) level topic nodes', () => {
-    expect(graph_from_text(
+    expect(graphFromText(
       '### First Topic \n### Second Topic \n### Third Topic'
     )).toEqual({
       "nodes": [
-        {
-          "id": "e00194285639baedc6dc178503819739df8fc8bd504bd83d115bb3f64c530d3a",
-          "name": "First Topic",
-          "node_type": 1,
-        },
-        {
-          "id": "6890869a8baf91bbf01ef426bcf38b638e13409df7ff490ddd05c68d6d09d2c8",
-          "name": "Second Topic",
-          "node_type": 1,
-        },
-        {
-          "id": "dcd9cbfb54e9dbcff23b828abf4d538224b0005d892beae877872c007eea3f14",
-          "name": "Third Topic",
-          "node_type": 1,
-        },
+        createTestNode(1, "First Topic", "e00194285639baedc6dc1785038", null, null, null),
+        createTestNode(1, "Second Topic", "6890869a8baf91bbf01ef4", null, null, null),
+        createTestNode(1, "Third Topic", "dcd9cbfb54e9dbcff2", null, null, null)
       ],
       edges: []
     })
   });
 
   it('nested topic nodes', () => {
-    expect(graph_from_text(
-      '# FirstTopic \n# SecondTopic \n## Second Topic Nested \n### Second Topic Nested Nested \n# Third Topic'
+    expect(graphFromText(
+      '# First Topic \n# SecondTopic \n## Second Topic Nested \n### Second Topic Nested Nested \n# Third Topic'
     )).toEqual({
-      nodes: [{
-        id: "22b99d04ec216612ca648389a1bc45b4229ba77bd5d131bc49022f18a6d04482",
-        name: "FirstTopic",
-        node_type: 1,
-      },
-      {
-        "id": "e211c01f4db8f6727eb99275b69d646ece818cfe2e6a0ce53c40010c3bf5168a",
-        "name": "SecondTopic",
-        "node_type": 1,
-      },
-      {
-        "id": "7f6a3d4c56d4201bb2fcda0f1b73e46022d5fb3655c5faa73da4de852418d2c2",
-        "name": "Second Topic Nested",
-        "node_type": 1,
-      },
-      {
-        "id": "b786e851cddca7fa4379fb7873c5b6a36b1bfc7603084d38b6e08b43a97b9dd2",
-        "name": "Second Topic Nested Nested",
-        "node_type": 1,
-      },
-      {
-        "id": "dcd9cbfb54e9dbcff23b828abf4d538224b0005d892beae877872c007eea3f14",
-        "name": "Third Topic",
-        "node_type": 1,
-      },
+      nodes: [
+        createTestNode(1, "First Topic", "e00194285639baedc6dc1785038", null, null, null),
+        createTestNode(1, "SecondTopic", "e211c01f4db8f6727eb99275b69d646ece", null, null, null),
+        createTestNode(1, "Second Topic Nested", "7f6a3d4c56d4201bb2fcda0f1b7", null, null, null),
+        createTestNode(1, "Second Topic Nested Nested", "b786e851cddca7fa4379f", null, null, null),
+        createTestNode(1, "Third Topic", "dcd9cbfb54e9dbcff23b828abf4d5382", null, null, null),
       ],
       edges: [
-        {
-          "id": "44b392290a2160bcf448693e013b409e0824c4f63f2e7b13239cf1dcfaf32ac2",
-          "source": "7f6a3d4c56d4201bb2fcda0f1b73e46022d5fb3655c5faa73da4de852418d2c2",
-          "target": "e211c01f4db8f6727eb99275b69d646ece818cfe2e6a0ce53c40010c3bf5168a",
-        },
-        {
-          "id": "acee519799f614a9d6ab1a8783c5e2b48309c1da80bdf9d7120dcb2321f0b901",
-          "source": "b786e851cddca7fa4379fb7873c5b6a36b1bfc7603084d38b6e08b43a97b9dd2",
-          "target": "7f6a3d4c56d4201bb2fcda0f1b73e46022d5fb3655c5faa73da4de852418d2c2",
-        },
+        createTestEdge("44b392290a2160bcf4", "7f6a3d4c56d4201bb2fcda", "e211c01f4db8f6727eb99275b6"),
+        createTestEdge("acee519799f614a9d6a", "b786e851cddca7fa437", "7f6a3d4c56d4201bb2fcda0f1"),
       ]
     })
   });
 
   it('mix of resource and topic nodes', () => {
-    expect(graph_from_text(
-      `# Topic1 \n- [Topic1Resource1Link](Link3)
-      \n## Topic2 Nested \n- Topic2NestResource1Desc 
-      \n## Topic2 Nested 2`
+    expect(graphFromText(
+      `# Topic1 \n- [Topic1ResLink](Link1)
+      \n## Topic2 \n- Topic2ResDesc 
+      \n## Topic2Nested`
     )).toEqual({
       nodes: [
-        {
-          "id": "ddc41a6256b8d5582119ff7b4b702d5956f912df3a0eaacecd50ab393d6f5bea",
-          "name": "Topic1",
-          "node_type": 1,
-        },
-        {
-          "data": {
-            "link": "Link3",
-            "text": "Topic1Resource1Link",
-          },
-          "id": "4a0b9d569b364e252ad9b055fa6e77d73e2ee87614a7b7c714551bc4397bb128",
-          "name": "Topic1Reso",
-          "node_type": 2,
-          "resource_type": 2,
-        },
-        {
-          "id": "6a89618a01641f4d0abf0905c8523ad2689cf645a044c3bc3234a3597999d388",
-          "name": "Topic2 Nested",
-          "node_type": 1,
-        },
-        {
-          "data": {
-            "link": "",
-            "text": "Topic2NestResource1Desc",
-          },
-          "id": "e4d4054a242ece026c4f4711b45068e535694794461649c21bbafbbc99964a6a",
-          "name": "Topic2Nest",
-          "node_type": 2,
-          "resource_type": 1,
-        },
-        {
-          "id": "fcfc46e167d8f04d0d142b9891cc35b1a861bdfc0b6ef581d57f72b340121227",
-          "name": "Topic2 Nested 2",
-          "node_type": 1,
-        },
+        createTestNode(1, "Topic1", "ddc41a6256b8d5582119ff7b4b702d5", null, null, null),
+        createTestNode(2, "Topic1ResL", "9d631c4a21315dafc31e8b15c47d", 2, "Topic1ResLink", "Link1"),
+        createTestNode(1, "Topic2", "508f75c1fd41d83b3a09121502965e8d", null, null, null),
+        createTestNode(2, "Topic2ResD", "85388daed79a483b01c67", 1, "Topic2ResDesc", ""),
+        createTestNode(1, "Topic2Nested", "8a95cfed41193f52443d0d5782b", null, null, null),
       ],
       edges: [
-        {
-          "id": "88479500733f77e49f943e18782a50c74e3feb418c8d4c0f9be97dd74572d391",
-          "source": "4a0b9d569b364e252ad9b055fa6e77d73e2ee87614a7b7c714551bc4397bb128",
-          "target": "ddc41a6256b8d5582119ff7b4b702d5956f912df3a0eaacecd50ab393d6f5bea",
-        },
-        {
-          "id": "42017da84ea3807ba0df9ae4c9d7263a4d733595b21232dbf8f6c0a7b965c293",
-          "source": "6a89618a01641f4d0abf0905c8523ad2689cf645a044c3bc3234a3597999d388",
-          "target": "ddc41a6256b8d5582119ff7b4b702d5956f912df3a0eaacecd50ab393d6f5bea",
-        },
-        {
-          "id": "4fb4116b612ba287b24a8b5142814b125a6dd4e5eb9c9b3faf746d10eec1f6f5",
-          "source": "e4d4054a242ece026c4f4711b45068e535694794461649c21bbafbbc99964a6a",
-          "target": "6a89618a01641f4d0abf0905c8523ad2689cf645a044c3bc3234a3597999d388",
-        },
-        {
-          "id": "496a801f39a547028cc7af69fa4308eb04cf5e4ddbc6bf44c771a64995c44389",
-          "source": "fcfc46e167d8f04d0d142b9891cc35b1a861bdfc0b6ef581d57f72b340121227",
-          "target": "ddc41a6256b8d5582119ff7b4b702d5956f912df3a0eaacecd50ab393d6f5bea",
-        },
+        createTestEdge("855b63c2a3f3d9c2fdc620fdb68", "9d631c4a21315dafc31e8b15c4", "ddc41a6256b8d5582119ff7b"),
+        createTestEdge("3902a3caa209498566943ed8aca", "508f75c1fd41d83b3a09121502", "ddc41a6256b8d5582119ff7b"),
+        createTestEdge("53d69247e0eae30dd3381af564e", "85388daed79a483b01c67b742a", "508f75c1fd41d83b3a091215"),
+        createTestEdge("b0ebf88195fe7ea87b07bd26e3e", "8a95cfed41193f52443d0d5782", "ddc41a6256b8d5582119ff7b"),
       ],
     });
   });
