af2bd4a800a90d5a9894b837f8abe57bad19a09f
Rustie (Ting-Chi) Lin <rustielin@berkeley.edu>
2020-07-07 21:32:44 -0700

    [utils] write IPFS shell wrapper for node

---
+++ utils/iok.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#
+# Various utilities
+#
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+show_help() {
+  echo "get_full_graph    Print full graph json from CID"
+}
+
+setup_node() {
+  if ! command -v node &> /dev/null; then
+    curl -sL https://deb.nodesource.com/setup_13.x | sudo bash -
+    sudo apt-get install -y nodejs
+    node -v
+  fi
+  (cd $DIR && npm install)
+}
+
+get_full_graph() {
+  cid=$1
+  if [ -z "$cid" ]; then
+    echo "CID not specified"
+    exit 1
+  fi
+  export NODE_NO_WARNINGS=1
+  export CID=$cid
+  export FUNC=getFullGraph
+  node --experimental-repl-await $DIR/ipfs_client.js
+}
+
+put_graph() {
+  read -r graph
+  if [ -z "$graph" ]; then
+    echo "Graph not specified"
+    exit 1
+  fi
+  export NODE_NO_WARNINGS=1
+  export GRAPH=$graph
+  export FUNC=putGraph
+  node --experimental-repl-await $DIR/ipfs_client.js
+}
+
+
+if declare -f "$1" >/dev/null 2>&1; then
+  "$@"
+else
+  show_help
+  exit 1
+fi
