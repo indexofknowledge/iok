e2e2f3c9b528639da274efaad2d7dcc21b7fb1f4
Rustie (Ting-Chi) Lin <rustielin@berkeley.edu>
2020-07-15 22:17:57 -0700

    [utils] pipenv setup for graph validator

--- utils/iok.sh
+++ utils/iok.sh
@@ -5,9 +5,11 @@
 #
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJ_DIR="$( cd "$( dirname "$DIR" )" >/dev/null 2>&1 && pwd )"
 
 show_help() {
   echo "setup_node        Install node if it\'s not present and npm install the utils"
+  echo "setup_py          Install pipenv if it\'s not present and build the pipenv"
   echo "get_full_graph    Print full graph json from CID"
   echo "put_graph         Takes graph JSON from stdin and puts to IPFS"
 }
@@ -19,6 +21,25 @@ setup_node() {
     node -v
   fi
   (cd $DIR && npm install)
+}
+
+setup_py() {
+  if ! command -v pipenv &> /dev/null; then
+    python3 -m pip install --upgrade pipenv
+  fi
+  (cd $PROJ_DIR/python && ./build.sh)
+}
+
+validate_graph() {
+  graph_file=$1
+  if [ -z $graph_file ]; then
+    echo "Graph file not specified"
+    exit 1
+  elif [ ! -s $graph_file ]; then
+    echo "Graph file missing or empty"
+    exit 1
+  fi
+  (cd $PROJ_DIR/python && pipenv run python3 -m validator --file $PROJ_DIR/$graph_file)
 }
 
 get_full_graph() {
