a71f1e4d56fe2b4d16a1fee79559308fa03e111f
Rustie (Ting-Chi) Lin <rustielin@berkeley.edu>
2020-07-25 17:26:14 -0700

    Updates

---
+++ precache-manifest.ec311d4ed1c17a082685ff14fd5990da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe23db97a80ded3817b7c214bf40581",
+    "url": "/index.html"
+  },
+  {
+    "revision": "997bf34bf716e1a2a663",
+    "url": "/static/css/2.09d1976d.chunk.css"
+  },
+  {
+    "revision": "7da2b7e2d7f3cb4d8351",
+    "url": "/static/css/main.2a0d2a17.chunk.css"
+  },
+  {
+    "revision": "997bf34bf716e1a2a663",
+    "url": "/static/js/2.7ef075f9.chunk.js"
+  },
+  {
+    "revision": "9dd5bb3999fe1388a73ae6ff00dabdfe",
+    "url": "/static/js/2.7ef075f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7da2b7e2d7f3cb4d8351",
+    "url": "/static/js/main.68725c24.chunk.js"
+  },
+  {
+    "revision": "ba3c88928723a424db3f",
+    "url": "/static/js/runtime-main.957cf8e1.js"
+  }
+]);
