20566d81b7e06af9cf1e13385f054a3cf590f39a
Rustie (Ting-Chi) Lin <rustielin@berkeley.edu>
2020-08-15 20:48:59 -0700

    [v4] node traversed toggle
    Adds actions and reducers for toggling node traversals. Traversals are
    stored in a separate Set from the actual IoK graph because we want the
    hash of the IoK graph to be consistent, and store all other metadata
    separately. In general:
    
    ```
    {
        graph: <IoK graph, resolves to canonical CID>,
        traversed: <list of traversed node IDs>,
        ...<other metadata>..
    }
    ```
    
    For test, added a button for debugging in `IokText`. We should change
    this into a more appropriate UI element.

--- v4/src/Sidebar.jsx
+++ v4/src/Sidebar.jsx
@@ -2,31 +2,35 @@ import { connect } from 'react-redux';
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  mergeNode, selectNode, selectMergeNode,
+  mergeNode, selectNode, selectMergeNode, toggleNodeTraversed
 } from './redux/actions';
 import IokText from './IokText';
 
 const mapStateToProps = (state) => ({
   selected: state.selected,
   mergingNode: state.mergingNode,
+  traversed: state.traversed,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   mergeNode: (fromId, toId) => dispatch(mergeNode(fromId, toId)),
   selectNode: (id) => dispatch(selectNode(id)),
   selectMergeNode: (id) => dispatch(selectMergeNode(id)),
+  toggleNodeTraversed: (id) => dispatch(toggleNodeTraversed(id)),
 });
 
-const FullSideBar = ({ mergingNode, selected }) => (
+const FullSideBar = ({ mergingNode, selected, traversed, toggleNodeTraversed }) => (
   <div>
-    {mergingNode ? <IokText node={mergingNode} /> : <div />}
-    <IokText node={selected} />
+    {mergingNode ? <IokText node={mergingNode} traversed={traversed} toggleNodeTraversed={toggleNodeTraversed}/> : <div />}
+    <IokText node={selected} traversed={traversed} toggleNodeTraversed={toggleNodeTraversed}/>
   </div>
 );
 
 FullSideBar.propTypes = {
   mergingNode: PropTypes.object, // eslint-disable-line
   selected: PropTypes.object, // eslint-disable-line
+  traversed: PropTypes.object, // eslint-disable-line
+  toggleNodeTraversed: PropTypes.func, // eslint-disable-line
 };
 
 const Sidebar = connect(mapStateToProps, mapDispatchToProps)(FullSideBar);
