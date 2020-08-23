774b773ad2535a74b9a49785d6d5defcbe8b354a
Rustie (Ting-Chi) Lin <rustielin@berkeley.edu>
2020-08-23 12:47:18 -0700

    [gha] move webapp build and deploy to gha

--- deploy_scraper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export SCRAPE_LINK="https://ipfs.io/ipfs/QmfA4hp3ue2JS2BR2hG18rWgVCWxDFaRYaGV4GQm4WsrdT"
-
-./build.sh
-
-pipenv run python3 -m scraper --link $SCRAPE_LINK --out ../README.md
